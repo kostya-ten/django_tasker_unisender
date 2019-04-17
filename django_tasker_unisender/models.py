f3bffd99d3d73587e7f5700011e633db89eaef37
kostya <kostya@yandex.ru>
2019-04-17 19:35:00 +0300

    fix

--- django_tasker_unisender/models.py
+++ django_tasker_unisender/models.py
@@ -6,51 +6,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class Unisender:
-    def __init__(self):
-        self.api_key = getattr(settings, 'UNISENDER_API_KEY', os.environ.get('UNISENDER_API_KEY'))
-        self.params = {'format': 'json', 'api_key': self.api_key}
-        self.url = "https://api.unisender.com/ru/api"
-
-    def create_list(self, title: str = None) -> int:
-        params = self.params
-        params['title'] = title
-
-        response = requests.get(
-            url="{url}/createList".format(url=self.url),
-            params=params
-        )
-
-        if response.status_code == 200:
-            json = response.json()
-            return json.get("result").get("id")
-
-    def delete_list(self, list_id: int) -> bool:
-        params = self.params
-        params['list_id'] = list_id
-        response = requests.get(
-            url="{url}/deleteList".format(url=self.url),
-            params=params,
-        )
-        if response.status_code == 200:
-            return True
-        else:
-            return False
-
-    def update_list(self, list_id: int, title: str) -> bool:
-        params = self.params
-        params['list_id'] = list_id
-        params['title'] = title
-        response = requests.get(
-            url="{url}/updateList".format(url=self.url),
-            params=params,
-        )
-        if response.status_code == 200:
-            return True
-        else:
-            return False
-
-
 class List(models.Model):
     title = models.CharField(max_length=200, verbose_name=_("Title"), unique=True)
 
@@ -62,16 +17,25 @@ class List(models.Model):
         verbose_name_plural = _("Lists")
 
     def delete(self, using=None, keep_parents=False):
-        unisender = Unisender()
-        unisender.delete_list(list_id=self.pk)
+        self._get_request(method='deleteList', data={'list_id': self.pk})
         super().delete(using, keep_parents)
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        unisender = Unisender()
-
         if not self.pk or force_insert:
-            self.pk = unisender.create_list(self.title)
+            result = self._get_request(method='createList', data={'title': self.title})
+            self.pk = result.get("result").get("id")
         else:
-            unisender.update_list(title=self.title, list_id=self.pk)
+            self._get_request(method='updateList', data={'list_id': self.pk})
 
         super().save(force_insert, force_update, using, update_fields)
+
+    def _get_request(self, method: str = None, data: dict = None) -> object:
+        url = "{url}/{method}".format(url="https://api.unisender.com/ru/api", method=method)
+        api_key = getattr(settings, 'UNISENDER_API_KEY', os.environ.get('UNISENDER_API_KEY'))
+        data = {**data, **{'format': 'json', 'api_key': api_key}}
+        response = requests.request(method='POST', url=url, data=data)
+
+        json = response.json()
+        if json.get('error'):
+            raise requests.HTTPError(json.get('error'))
+        return json

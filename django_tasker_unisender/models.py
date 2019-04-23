a37d02d15e08b9170be79ddfd6c3086a84e86d05
kostya <kostya@yandex.ru>
2019-04-23 08:49:18 +0300

    fix

--- django_tasker_unisender/models.py
+++ django_tasker_unisender/models.py
@@ -1,4 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from .unisender import Unisender
 
 
@@ -28,6 +32,6 @@ class EmailModel(models.Model):
 
 
 # Signals
-# @receiver(pre_save, sender=User)
-# def modelList(instance=None, **kwargs):
-#     pass
+@receiver(post_save, sender=User)
+def modelList(instance=None, **kwargs):
+    pass
